dc440ce4d2d862b52ac3ca406a3750cce74018cc
Ana Green <greena22@tcnj.edu>
2023-12-13 18:13:01 -0500

    Orbit Animation Update

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// Setup //
+// // Setup // //
 
 // Scene
 const scene = new THREE.Scene();
+
+// Auto Orbit
+const autoOrbit = false;
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -18,23 +21,55 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setX(-30);
+camera.position.setY(0);
 
 // Render = Draw
 renderer.render(scene, camera);
 
-// Objects //
+// // Objects // //
 
-// Creates a geometric object (Torus)
-const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshStandardMaterial({color:0xFF6347});
+// Center of Universe Object
+//const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+//const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+// Creates Sphere and adds it to scene
+//const moon = new THREE.Mesh(
+//  new THREE.SphereGeometry(3,32,32),
+//  new THREE.MeshStandardMaterial({map: moonTexture, normalMap: normalTexture})
+//);
+//scene.add(moon);
+
+const geometry = new THREE.SphereGeometry(3,32,32);
+const material = new THREE.MeshStandardMaterial({color:0x555555});
 //const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true}); // Wireframe version
-const torus = new THREE.Mesh(geometry, material);
+const centerObject = new THREE.Mesh(geometry, material);
+
+// Sets position
+centerObject.position.x = 10;
+centerObject.position.z = -20;
 
 // Adds it to the scene
-scene.add(torus);
+scene.add(centerObject);
 
-// Lights //
+
+// Orbit Object //
+const orbitGeometry = new THREE.TorusGeometry(75,1,10,100);
+const orbitMaterial = new THREE.MeshBasicMaterial({color:0xffffff, wireframe:true}); // Wireframe version
+const orbitPath = new THREE.Mesh(orbitGeometry, orbitMaterial);
+
+orbitPath.rotation.x = 55;
+scene.add(orbitPath);
+
+// Creates Torus
+const torusGeometry = new THREE.TorusGeometry(10,3,16,100);
+const torusMaterial = new THREE.MeshStandardMaterial({color:0xFF6347});
+const torusObject = new THREE.Mesh(torusGeometry, torusMaterial);
+
+torusObject.position.x = 40;
+torusObject.position.z = 50;
+scene.add(torusObject);
+
+// // Lights // //
 
 //Creates some lights and adds them to the scene
 const pointLight = new THREE.PointLight(0xffffff);
@@ -58,7 +93,7 @@ function addStar() {
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry,material);
 
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(250));
 
   star.position.set(x,y,z);
   scene.add(star);
@@ -67,27 +102,50 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
-// Background
-//const bgTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = new THREE.Color( 0x431a38 );
+// Background Image
+const bgTexture = new THREE.TextureLoader().load('spaceblue.png');
+//scene.background = new THREE.Color( 0x000000 );
+scene.background = new THREE.TextureLoader().load('spaceblue.png');
+
+
+// // Content Objects // //
+
+
+// Content Panel //
+
+
+const proj1 = new THREE.Mesh(
+  new THREE.BoxGeometry(7,5,.1),
+  new THREE.MeshBasicMaterial({color:0x222222})
+);
+scene.add(proj1);
+
+proj1.position.z = 23;
+proj1.position.x = -5;
+
 
 // Avatar Box //
 
-// Avatar Box Image
+// Texture Image
 const avatarTexture = new THREE.TextureLoader().load('avatar.png');
 
+// Creates Box and adds it to scene
 const avatarBox = new THREE.Mesh(
     new THREE.BoxGeometry(3,3,3),
     new THREE.MeshBasicMaterial({map: avatarTexture})
 );
 scene.add(avatarBox);
+
+// Avatar Box Position
 avatarBox.position.z = 23;
 avatarBox.position.x = 4;
 
 // Moon //
 
+// Texture + Normal map
 const moonTexture = new THREE.TextureLoader().load('moon.jpg');
 const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+// Creates Sphere and adds it to scene
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3,32,32),
   new THREE.MeshStandardMaterial({map: moonTexture, normalMap: normalTexture})
@@ -98,15 +156,57 @@ scene.add(moon);
 moon.position.z = 28;
 moon.position.setX(-10);
 
+// Project Box 1 - Games //
+const proj1Texture = new THREE.TextureLoader().load('avatar.png');
+
+const proj1Box = new THREE.Mesh(
+    new THREE.BoxGeometry(5,5,5),
+    new THREE.MeshBasicMaterial({map: proj1Texture})
+);
+
+proj1Box.position.z = 36;
+proj1Box.position.x = 4;
+
+// Project Box 2 - VR/AR //
+const proj2Texture = new THREE.TextureLoader().load('avatar.png');
+
+const proj2Box = new THREE.Mesh(
+    new THREE.BoxGeometry(5,5,5),
+    new THREE.MeshBasicMaterial({map: proj2Texture})
+);
+
+proj2Box.position.z = 46;
+proj2Box.position.x = -6;
+
+// Project Box 3 - 3D stuff //
+const proj3Texture = new THREE.TextureLoader().load('avatar.png');
+
+const proj3Box = new THREE.Mesh(
+    new THREE.BoxGeometry(5,5,5),
+    new THREE.MeshBasicMaterial({map: proj3Texture})
+);
+
+proj3Box.position.z = 60;
+proj3Box.position.x = 6;
+
+scene.add(proj1Box,proj2Box,proj3Box);
+
+
 // Scroll animation
 function moveCamera() {
 
   const t = document.body.getBoundingClientRect().top;
 
   // Moves Camera while scrolling
-  camera.position.z = t * -0.01;
+  /*camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
+  camera.position.y = t * -0.0002;*/
+  
+
+  // Position on Orbit //
+  if(!autoOrbit){orbitPath.rotation.z = t * 0.001;}
+
+  
 
   // Changes Moon Position while scrolling
   moon.rotation.x += 0.05;
@@ -116,22 +216,47 @@ function moveCamera() {
   avatarBox.rotation.y += 0.01;
   avatarBox.rotation.z += 0.01;
 
+  
+
 }
 document.body.onscroll = moveCamera;
+
+// // Different Camera Viewpoints // //
+
+// About Camera //
+function aboutCamera() {
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 0;
+}
+
+function worksPageCamera() {
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 0;
+
+  autoOrbit = true;
+}
+
+
+
+// // Renders Scene // //
 
 // Recursive function that animates the scene
 function animate() {
     requestAnimationFrame(animate);
 
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    torusObject.rotation.x += 0.01;
+    torusObject.rotation.y += 0.005;
+    torusObject.rotation.z += 0.01;
+
+    if(autoOrbit){orbitPath.rotation.z += 0.001;}
 
     controls.update();
 
     renderer.render(scene, camera);
 }
 
-animate()
+animate();
 
 
