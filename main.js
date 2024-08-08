270d702d0149d5d1ed08dabaa63338991cbdde13
Ana Green <greena22@tcnj.edu>
2024-08-08 01:45:11 -0400

    Color Palette

--- main.js
+++ main.js
@@ -15,46 +15,10 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 /* Mobile Hamburger Menu */
-//function hamMenu() {
-  const hamMenu = document.querySelector('#ham');
-  const offScreenMenu = document.querySelector('.off-screen-menu');
+const hamMenu = document.querySelector('#ham');
+const offScreenMenu = document.querySelector('.off-screen-menu');
 
-  hamMenu.addEventListener('click',()=> {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-  })
-  
-//}
-
-
-
-/*
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    
-  </div>
-`
-setupCounter(document.querySelector('#counter'))
-
-
-
-
-
-// Copy Email Button
-const emailAdd = "ana.daren.green@gmail.com"
-function copyEmail() {
-  // Get the text field
-  var copyText = emailAdd;
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(emailAdd);
-
-  // Alert the copied text
-  alert("Copied the text: " + emailAdd);
-} 
-
-*/
+hamMenu.addEventListener('click',()=> {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
