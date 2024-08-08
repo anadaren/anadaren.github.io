0afcf7294524a06135ee21a92e66f88688b15132
Ana Green <greena22@tcnj.edu>
2024-08-07 23:37:23 -0400

    Hamburger Menu

--- main.js
+++ main.js
@@ -15,38 +15,17 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 /* Mobile Hamburger Menu */
-function hamMenu() {
-  const hamMenu = document.querySelector('.ham');
+//function hamMenu() {
+  const hamMenu = document.querySelector('#ham');
   const offScreenMenu = document.querySelector('.off-screen-menu');
 
-  hamMenu.classList.toggle('active');
-  offScreenMenu.classList.toggle('active');
-}
+  hamMenu.addEventListener('click',()=> {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+  })
+  
+//}
 
-
-/* Scroll to top button */
-
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
-
-/* End of Scroll Button */
 
 
 /*
