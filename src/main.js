c76a9d46b89c45ef4e33261dd4b5fa4a628a3903
Ana Green <greena22@tcnj.edu>
2024-09-27 10:17:29 -0400

    CSS Tweaks

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
 /* Scroll Animations */
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if(entry.isIntersecting) {
+    // Checks if element is currently visible on screen
+    if(entry.isIntersecting) {    // Adds class 'show' when element is visible
       entry.target.classList.add('show');
-    } else {
+    } else {  // Removes class 'show' when element is not visible
       entry.target.classList.remove('show');
     }
   })
-},
-{ threshold: 0.2
 });
 
 // Hides hidden elements before they are scrolled to
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
-
-//
-for (let i = 0; i < the_animation.length; i++) {
-const elements = the_animation[i];
-
- observer.observe(elements);
-}
