c38472c38cf00a14ccc541ff703934222945d188
Ana Green <greena22@tcnj.edu>
2025-01-09 15:24:13 -0500

    Skills section revamp

--- src/main.js
+++ src/main.js
@@ -52,3 +52,15 @@ const openTab = (tabName) => {
 }
 
 
+/* Cursor Glow */
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', moveCursor)
